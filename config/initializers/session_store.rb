2bc9e99384efacc5d34b55b4ec33bf96fbc2b4e3
chinacheng <chinachengliwen@gmail.com>
2010-09-28 17:31:24 +0800

    修改配置文件、忽略列表以及配置文件

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+
+#  ActionController::Base.session = {
+#    :domain => "domain.com", 
+#    :key=>'key_str',
+#    :secret=>'secret_str'
+#  }
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,19 +1,34 @@
+# worker 数量
 worker_processes 3
 
-listen '/tmp/nginx/sockets/discuss_unicorn.sock', :backlog => 2048
-timeout 30
+# 日志位置
+stderr_path("/web/2010/logs/unicorn-discuss-error.log")
+stdout_path("/web/2010/logs/unicorn-discuss.log")
 
-pid_file = "/web/2010/pids/unicorn_pin_discuss.pid"
-pid pid_file
+# 加载 超时设置 监听
 preload_app true
+timeout 30
+listen '/web/2010/sockets/unicorn-discuss.sock', :backlog => 2048
+
+pid_file_name = "/web/2010/pids/unicorn-discuss.pid"
+pid pid_file_name
+
+# REE GC
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end
 
 before_fork do |server, worker|
-  old_pid = pid_file + '.oldbin'
+  old_pid = pid_file_name + '.oldbin'
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
+      # ...
     end
   end
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
 end
