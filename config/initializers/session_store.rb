8a5c75b9b910b320a99ed95a7ec513e889cbcfe1
chinacheng <chinachengliwen@gmail.com>
2010-09-29 11:04:20 +0800

    session_store的修改

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-#  ActionController::Base.session = {
-#    :domain => "domain.com", 
-#    :key=>'key_str',
-#    :secret=>'secret_str'
-#  }
+  ActionController::Base.session = {
+    :domain => "2010.mindpin.com", 
+    :key=>'_mindpin_2010_session_dev',
+    :secret=>'883abe7844502ee307e376fa4d0509253d7f9e55fc8be69a934735cd470cc8671af39e27482885960f3364fa8af420b5519571193e22987c3e9e4f9da29f15fb'
+  }
